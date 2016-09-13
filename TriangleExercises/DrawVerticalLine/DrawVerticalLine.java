8a746e3cc4b563dd20b233b159d622553a2e5e9f
rhuanhuan <1210098674@qq.com>
2016-09-13 11:19:19 +0800

    Finish draw vertival line function

--- TriangleExercises/DrawVerticalLine/DrawVerticalLine.java
+++ TriangleExercises/DrawVerticalLine/DrawVerticalLine.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class DrawVerticalLine{
     public static void main(String args[]){
-
+        Scanner scan = new Scanner(System.in);
+        int read = scan.nextInt();
+        int i = 0;
+        for (i = 0; i < read; i++) {
+            System.out.println('*');
+        }
     }
 }
