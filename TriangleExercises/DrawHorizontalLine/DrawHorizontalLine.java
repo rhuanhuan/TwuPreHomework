923771c53a71dc385b9b076f2828d8ce033f500a
rhuanhuan <1210098674@qq.com>
2016-09-13 10:59:41 +0800

    Out put triangle line

---
+++ TriangleExercises/DrawHorizontalLine/DrawHorizontalLine.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DrawHorizontalLine{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int read = scan.nextInt();
+        int i = 0;
+        for (i = 0; i < read; i++) {
+            System.out.print('*');
+        }
+        System.out.print('\n');
+    }
+}
