b15d1a8d88a5a77f59ebff79e5d77bcc1198976c
rhuanhuan <1210098674@qq.com>
2016-09-13 15:22:53 +0800

    Diamond with name function

---
+++ DiamondExercise/NameDiamond/NameDiamond.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NameDiamond {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int read = scan.nextInt();
+        int i = 0, j = 0;
+        for (i = 0; i < 2 * read - 1; i++) {
+            if (i == read-1) {
+                System.out.println("Huan Ruan");
+            }
+            else{
+                for (j = 0; j < Math.abs(read - i - 1); j++) {
+                    System.out.print(' ');
+                }
+                for (j = Math.abs(read - i - 1); j < 2 * read - Math.abs(read - i - 1) - 1; j++) {
+                    System.out.print('*');
+                }
+                System.out.print('\n');
+            }
+
+        }
+    }
+}
